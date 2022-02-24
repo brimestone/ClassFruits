1a0759467763577cf9cf68a7c8e730a7b3098020
Ted Butler <tedsbutler@gmail.com>
2022-02-24 14:44:58 -0500

    Initial Commit

---
+++ Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by Ted Butler on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
