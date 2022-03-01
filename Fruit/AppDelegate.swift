9265bb421d200878b4aca9a9b2303f6831916255
Ted Butler <tedsbutler@gmail.com>
2022-03-01 12:10:11 -0500

    allow alert even if app is foreground

--- Fruit/AppDelegate.swift
+++ Fruit/AppDelegate.swift
@@ -15,6 +15,14 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         return true
     }
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+            //If you don't want to show notification when app is open, do something here else and make a return here.
+            //Even you you don't implement this delegate method, you will not see the notification on the specified controller. So, you have to implement this delegate and make sure the below line execute. i.e. completionHandler.
+
+            completionHandler([.banner, .badge, .sound])
+    }
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         let userInfo = response.notification.request.content.userInfo
 
